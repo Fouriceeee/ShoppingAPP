b88916ee4fa29f15f699eae642a790fed5c829b0
IronSword <thriceee@163.com>
2025-05-31 13:37:40 +0800

    JsonIO, some Users and Products

--- src/main/java/org/example/JsonIO.java
+++ src/main/java/org/example/JsonIO.java
@@ -1,69 +1,47 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 
 public class JsonIO {
-    public static ArrayList<User> readUsersFromJson(String filePath) throws IOException {
-        try(JsonReader reader = new JsonReader(new FileReader(filePath))) {
-            ArrayList<User> users = new ArrayList<>();
-            reader.beginArray();
-            while (reader.hasNext()) {
-                JsonElement jsonElement = com.google.gson.JsonParser.parseReader(reader);
-                users.add(getUser(jsonElement));
-            }
-            reader.endArray();
-            return users;
+    //UserIO
+    public static User[] readUsers(String filePath) throws IOException {
+        try(Reader reader = new FileReader(filePath)) {
+            Gson gson = new Gson();
+            return gson.fromJson(reader,User[].class);
         }
     }
 
-    public static ArrayList<Product> readProductsFromJson(String filePath) throws IOException {
-        try(JsonReader reader = new JsonReader(new FileReader(filePath))) {
-            ArrayList<Product> products = new ArrayList<>();
-            reader.beginArray();
-            while (reader.hasNext()) {
-                JsonElement jsonElement = com.google.gson.JsonParser.parseReader(reader);
-                products.add(getProduct(jsonElement));
-            }
-            reader.endArray();
-            return products;
+    public static void writeUsers(User[] users,String filePath) throws IOException{
+        try(JsonWriter writer = new JsonWriter(new FileWriter(filePath))) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(users,User[].class,writer);
         }
     }
 
-    private static User getUser(JsonElement jsonElement) {
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-        User user = new User();
-
-        user.setId(jsonObject.get("ID").getAsLong());
-        user.setName(jsonObject.get("NAME").getAsString());
-        user.setEmail(jsonObject.get("E-MAIL").getAsString());
-        user.setPassword(jsonObject.get("PASSWORD").getAsString());
-
-        if(jsonObject.get("GROUP").getAsString().equals("ADMIN")) {
-            user.setGroup(Group.ADMIN);
+    //ProductIO
+    public static Product[] readProducts(String filePath) throws IOException {
+        try(Reader reader = new FileReader(filePath)) {
+            Gson gson = new Gson();
+            return gson.fromJson(reader,Product[].class);
         }
-        else {
-            user.setGroup(Group.CUSTOMER);
-        }
-        return user;
     }
 
-    private static Product getProduct(JsonElement jsonElement) {
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-        Product product = new Product();
-
-        product.setName(jsonObject.get("NAME").getAsString());
-        product.setCategory(jsonObject.get("CATEGORY").getAsString());
-        product.setInventory(jsonObject.get("INVENTORY").getAsInt());
-        product.setPrice(jsonObject.get("PRICE").getAsLong());
-
-        return product;
+    public static void writeProducts(Product[] products,String filePath) throws IOException{
+        try(JsonWriter writer = new JsonWriter(new FileWriter(filePath))) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(products,Product[].class,writer);
+        }
     }
+
 }
