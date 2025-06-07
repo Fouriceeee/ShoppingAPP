15e183ae093f02d5aace0e2a66c519b8e40b7007
IronSword <thriceee@163.com>
2025-06-07 15:33:45 +0800

    新方法Category.containsEnumValue() 新方法ProductController.addToProducts() 新内部类ProductControlle.AddToProductsRequest

--- backend/src/main/java/org/example/model/Category.java
+++ backend/src/main/java/org/example/model/Category.java
@@ -1,5 +1,14 @@
 package org.example.model;
 
 public enum Category {
-    VIDEOCARD,CPU,FRUIT,DRINK,SNACK
+    VIDEOCARD,CPU,FRUIT,DRINK,SNACK;
+
+    public static boolean containsEnumValue(Class<? extends Enum<?>> enumClass, String value) {
+        for (Enum<?> enumConstant : enumClass.getEnumConstants()) {
+            if (enumConstant.name().equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
