a546d1046966853a46df84522855a2ae0a5d1b9f
IronSword <thriceee@163.com>
2025-06-07 16:27:41 +0800

    注册ProductController.addToProducts()路由

--- backend/src/main/java/org/example/model/Category.java
+++ backend/src/main/java/org/example/model/Category.java
@@ -1,7 +1,7 @@
 package org.example.model;
 
 public enum Category {
-    VIDEOCARD,CPU,FRUIT,DRINK,SNACK;
+    VIDEOCARD,CPU,FRUIT,DRINK,SNACK,PAD,LAPTOP;
 
     public static boolean containsEnumValue(Class<? extends Enum<?>> enumClass, String value) {
         for (Enum<?> enumConstant : enumClass.getEnumConstants()) {
