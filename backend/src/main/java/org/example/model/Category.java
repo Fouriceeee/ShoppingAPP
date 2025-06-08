2879bfce18a7a5b9641a1c70b2652122e456be82
IronSword <thriceee@163.com>
2025-06-08 19:28:44 +0800

    新商品和新Category

--- backend/src/main/java/org/example/model/Category.java
+++ backend/src/main/java/org/example/model/Category.java
@@ -1,7 +1,7 @@
 package org.example.model;
 
 public enum Category {
-    VIDEOCARD,CPU,MOTHERBOARD,RAM,STORAGE,POWERSUPPLY, COOLING,CASE,PERIPHERALS,PAD,MONITOR,LAPTOP;
+    VIDEOCARD,CPU,MOTHERBOARD,RAM,STORAGE,POWERSUPPLY, COOLING,CASE,PERIPHERALS,PAD,MONITOR,LAPTOP,GRAPHIC_TABLET;
 
     public static boolean containsEnumValue(Class<? extends Enum<?>> enumClass, String value) {
         for (Enum<?> enumConstant : enumClass.getEnumConstants()) {
